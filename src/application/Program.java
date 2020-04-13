2cbeb91bbecb6c94c08ac4a38ce77bbf93e987cf
MoysesPietsch <moyzlevi@hotmail.com>
2020-04-13 15:27:41 -0300

    just bad

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,14 +35,11 @@ public class Program {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 			
-			Date now = new Date();
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future");
-			}else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: check-out date must be after check-in");
+			String error = reservation.updateDates(checkIn, checkOut);
+			if (error != null) {
+				System.out.println("Error in reservation: " + error);
 			}
-			else {
-				reservation.updateDates(checkIn, checkOut);
+			else {	
 				System.out.println("Reservation: " + reservation);
 			}
 			
@@ -52,5 +49,5 @@ public class Program {
 	
 	
 	}
-
 }
+
